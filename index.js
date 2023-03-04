ba283fa7c8f775eee2dd6b072019a1f1d7471554
xianshenglu <xianshenglu@qq.com>
2023-03-04 14:04:12 +0800

    feat: add codePush

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import { registerRootComponent } from 'expo';
-
+import codePush from "react-native-code-push";
 import App from './App';
 
+loadDevEnv();
+
+const codePushOptions = {
+  updateDialog: true,
+  deploymentKey: process.env.CODE_PUSH_DEPLOYMENT_KEY,
+};
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(codePush(codePushOptions)(App));
+
+function loadDevEnv() {
+  if (!process.env.NODE_ENV) {
+    dotenv.config();
+  }
+}
