8149d33274e307e613816f51341a7719abee3213
xianshenglu <xianshenglu@qq.com>
2023-03-08 22:34:37 +0800

    chore: add translation for update message

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import { registerRootComponent } from 'expo';
 import codePush from "react-native-code-push";
 import App from './App';
 import { CODE_PUSH_DEPLOYMENT_KEY } from "@env";
+import { AppI18n } from './localize';
 
 const codePushOptions = {
-  updateDialog: true,
+  updateDialog: {
+    optionalUpdateMessage : AppI18n.t('update.optionalUpdateMessage'),
+    optionalInstallButtonLabel : AppI18n.t('update.optionalInstallButtonLabel'),
+    optionalIgnoreButtonLabel : AppI18n.t('update.optionalIgnoreButtonLabel'),
+    title: AppI18n.t('update.title'),
+  },
   deploymentKey: CODE_PUSH_DEPLOYMENT_KEY,
 };
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
