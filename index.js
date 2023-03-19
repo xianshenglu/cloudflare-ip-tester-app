56ad500f0400dc88f6e6322fad15b6fb10e8a484
xianshenglu <xianshenglu@qq.com>
2023-03-19 16:16:07 +0800

    chore: add translation for code push forced update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 import codePush from "react-native-code-push";
-import App from './App';
+import App from "./App";
 import { CODE_PUSH_DEPLOYMENT_KEY } from "@env";
-import { AppI18n } from './localize';
+import { AppI18n } from "./localize";
 
 const codePushOptions = {
   updateDialog: {
-    optionalUpdateMessage : AppI18n.t('update.optionalUpdateMessage'),
-    optionalInstallButtonLabel : AppI18n.t('update.optionalInstallButtonLabel'),
-    optionalIgnoreButtonLabel : AppI18n.t('update.optionalIgnoreButtonLabel'),
-    title: AppI18n.t('update.title'),
+    optionalUpdateMessage: AppI18n.t("update.optionalUpdateMessage"),
+    optionalInstallButtonLabel: AppI18n.t("update.optionalInstallButtonLabel"),
+    optionalIgnoreButtonLabel: AppI18n.t("update.optionalIgnoreButtonLabel"),
+    title: AppI18n.t("update.title"),
+    mandatoryUpdateMessage: AppI18n.t("update.mandatoryUpdateMessage"),
+    mandatoryContinueButtonLabel: AppI18n.t("update.mandatoryContinueButtonLabel"),
   },
   deploymentKey: CODE_PUSH_DEPLOYMENT_KEY,
 };
